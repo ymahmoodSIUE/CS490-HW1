6894d798f117d759b0e291e8daa007a5cb8cb601
ymahmoodSIUE <123699824+ymahmoodSIUE@users.noreply.github.com>
2024-01-27 13:54:44 -0600

    Problem1-install_sft.sh
    Updated install script

--- Problem1-install_sft.sh
+++ Problem1-install_sft.sh
@@ -6,3 +6,10 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
+
+if [ -f ~/miniconda3/bin/conda ]; then
+        ~/miniconda3/bin/conda
+        echo "the miniconda3 has been found"
+else
+        echo "No miniconda3"
+fi
